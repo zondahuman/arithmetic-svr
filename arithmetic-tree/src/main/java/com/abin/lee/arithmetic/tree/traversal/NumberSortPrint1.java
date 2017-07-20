26b66afad6ec6e8a948edfac79706c7eb9200175
zondahuman <zondahuman@gmail.com>
2017-07-20 23:48:57 +0800

    add

--- arithmetic-tree/src/main/java/com/abin/lee/arithmetic/tree/traversal/NumberSortPrint1.java
+++ arithmetic-tree/src/main/java/com/abin/lee/arithmetic/tree/traversal/NumberSortPrint1.java
@@ -6,43 +6,42 @@ package com.abin.lee.arithmetic.tree.traversal;
  * com.abin.lee.arithmetic.tree.traversal
  */
 public class NumberSortPrint1 {
-    static final Object lock = new Object();
+    static final NumberSortPrint1 lock = new NumberSortPrint1();
     public static void main(String[] args) {
-        final Thread t1 = new Thread(new Runnable() {
-            int i = -1;
-            public void run() {
-                while(i<99){
-                    synchronized (lock){
-                        System.out.println("Thread1: "+ Thread.currentThread().getName() +", "+ (i+=2));
-                        lock.notifyAll();
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+        new Thread(new Number1()).start();
+        new Thread(new Number2()).start();
+    }
+}
+
+class Number1 implements Runnable{
+    int i = -1;
+    public void run() {
+        while(i<99){
+            synchronized (NumberSortPrint1.lock){
+                System.out.println("Thread-Name: "+ Thread.currentThread().getName() +", "+ (i+=2));
+                NumberSortPrint1.lock.notify();
+                try {
+                    NumberSortPrint1.lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-        });
-        t1.setName("Thread1");
-        t1.start();
-        final Thread t2 = new Thread(new Runnable() {
-            int i = 0;
-            public void run() {
-                while(i<99){
-                    synchronized (lock){
-                        System.out.println("Thread2: "+ Thread.currentThread().getName() +", "+ (i+=2));
-                        lock.notifyAll();
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+        }
+    }
+}
+class Number2 implements Runnable{
+    int i = 0;
+    public void run() {
+        while(i<99){
+            synchronized (NumberSortPrint1.lock){
+                System.out.println("Thread-Name: "+ Thread.currentThread().getName() +", "+ (i+=2));
+                NumberSortPrint1.lock.notify();
+                try {
+                    NumberSortPrint1.lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-        });
-        t1.setName("Thread2");
-        t2.start();
+        }
     }
 }
